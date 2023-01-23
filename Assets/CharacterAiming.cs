39a09acda1958e876ef767f3c7d22800caf38dea
Kees Rodriguez <kees.rodriguez@gmail.com>
2023-01-23 22:36:46 +0100

    video 2
    [#02] THIRD PERSON SHOOTER: Character Strafe Locomotion (Blend Trees + Cinemachine)

--- Assets/CharacterAiming.cs
+++ Assets/CharacterAiming.cs
@@ -7,6 +7,8 @@ public class CharacterAiming : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         mainCamera = Camera.main;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
